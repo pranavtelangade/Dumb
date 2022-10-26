2c36ddef4ea55fd466f33edd7aa298f198b6affe
Pranav Telangade <19305011pranav@viva-technology.org>
2022-10-26 17:52:25 +0530

    Update Main

---
+++ assets/js/dorang.js
@@ -0,0 +1,57 @@
+ // toggle 
+$(document).ready(function(){
+    
+    $('.search-toggle').click(function(){
+        $('.search-wrapper').toggleClass('show');
+    });
+
+    $('.modal-toggle').click(function(){
+        $('.modalBox').toggleClass('show');
+    })
+
+    $('.modalBox').click(function(){
+        $(this).removeClass('show');
+    });
+
+    $('.spinner').click(function(){
+        $(".theme-selector").toggleClass('show');
+    });
+    $('.light').click(function(){
+        $('body').addClass('light-theme');
+        $('body').removeClass('dark-theme');
+    });
+    $('.dark').click(function(){
+        $('body').toggleClass('dark-theme');
+        $('body').removeClass('light-theme');
+    });
+});
+
+
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+}); 
+
+// button function
+var btn = document.querySelector(".btn");
+var position;
+btn.addEventListener("mouseover", function(){
+    position ? (position = 0) : (position = 200);
+    btn.style.transform = `translate(${position}px, 0px)`;
+    btn.style.transition = "all 0.2s ease";
+});
